45158793adf6823ff0145728ff66f3fc1d80e2b5
Daniel Orozco <domarenon@gmail.com>
2022-10-04 16:38:54 -0500

    adding changing price functionality

--- assets/card-gradiproducts.js
+++ assets/card-gradiproducts.js
@@ -3,6 +3,7 @@ let optionColor = document.querySelectorAll("optionColor");
 optionColor.forEach(function (item, index) {
     item.addEventListener('click', function(){   
         changeImage(this);
+        changePrice(this);
         let allOptions = getClosestOptionColors(this);
         for (let i=0; i < allOptions.length; i++) {
             allOptions[i].classList.remove("selected");
@@ -30,20 +31,32 @@ optionColor.forEach(function (item, index) {
     });
 });
 
+function changePrice(optionColorElement){
+    let dataColor = getDataColor(optionColorElement);
+    let productElement = optionColorElement.closest('.card--media')
+    let prices = productElement.querySelectorAll(".price")
+    for (let i=0; i < prices.length; i++) {
+        prices[i].classList.remove("price-selected");
+        if (dataColor == prices[i].getAttribute('data-color')){
+            prices[i].classList.add("price-selected");
+        }
+    }
+}
+
 function changeImage(optionColorElement){
-    let dataImage = getDataImage(optionColorElement);
+    let dataColor = getDataColor(optionColorElement);
     let productElement = optionColorElement.closest('.card--media')
     let productMedias = productElement.querySelector('.card__inner')
-    let allMedias = productMedias.querySelectorAll("[data-image]")
+    let allMedias = productMedias.querySelectorAll("[data-color]")
     for (let i=0; i < allMedias.length; i++) {
         allMedias[i].setAttribute("style", "display:none;");
     }
-    let thisElement = productMedias.querySelectorAll("[data-image='"+ dataImage +"']")
+    let thisElement = productMedias.querySelectorAll("[data-color='"+ dataColor +"']")
     thisElement[0].setAttribute("style", "display:block;");
 }
 
-function getDataImage(element) {
-    let val = element.getAttribute("data-image");  
+function getDataColor(element) {
+    let val = element.getAttribute("data-color");  
     return val;
 }
 
