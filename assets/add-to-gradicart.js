bb67f7013923b959accc3b438f11b99a10651afa
Daniel Orozco <domarenon@gmail.com>
2022-10-05 11:14:48 -0500

    adding api request to add to cart

--- assets/add-to-gradicart.js
+++ assets/add-to-gradicart.js
@@ -1,31 +1,46 @@
-let addToCart = document.querySelectorAll("gradi-add-to-cart")
-let formData = {
-    'items': [{
-     'id': null,
-     'quantity': 1
-     }]
-   };
+let addToCart = document.querySelectorAll(".gradi-add-to-cart")
+
 addToCart.forEach(function (item, index) {
-    item.addEventListener('click', function(){  
-        formData.items.id = this.data-product-id;
-        if (this.quantity){
-            formData.items.quantity = this.quantity;
+    item.addEventListener('click', function(){
+
+        let variantId = this.getAttribute('data-product-id');
+        let quantity = 1;
+        if (this.hasAttribute('quantity')){
+             quantity= this.getAttribute('quantity');
         }
 
-        console.log(JSON.stringify(formData))
+        let formData = {
+            'items': [{
+            'id': variantId,
+            'quantity': quantity
+            }]
+        };
 
-        fetch(window.Shopify.routes.root + 'cart/add.js', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-          })
-          .then(response => {
-            return response.json();
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-          });
+        if(variantId !== ""){
+            fetch(window.Shopify.routes.root + 'cart/add.js', {
+                method: 'POST',
+                credentials: 'same-origin',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'X-Requested-With':'xmlhttprequest'
+                },
+                body: JSON.stringify(formData)
+              })
+              .then(response => {
+                if(response.ok){
+                    window.location.href = window.location.origin+"/cart"
+                }
+                return response.json();
+              })
+              .then(result =>{
+                if(result.description){
+                    alert(result.description)
+                }
+              })
+              .catch((error) => {
+                console.log('Error:', error);
+              });
+        }
+        
     });
 });
