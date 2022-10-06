82c22416effbbb8e04f420e770eebcb3b8049135
Daniel Orozco <domarenon@gmail.com>
2022-10-06 11:47:31 -0500

    adding responsive design to slider

--- assets/slider-gradiproduct.js
+++ assets/slider-gradiproduct.js
@@ -3,17 +3,19 @@
     const arrowNext = document.querySelector('#next')
     const arrowPrev = document.querySelector('#prev')
 
+
     arrowNext.addEventListener('click', () => changePosition(1))
     arrowPrev.addEventListener('click', () => changePosition(-1))
 
     function changePosition(change){
+        
         const currentElement = Number(document.querySelector('.slider__body--show').dataset.id);
         let value = currentElement + change;
         if (value === 0 || value == sliders.length+1){
             value = value === 0 ? sliders.length : 1
         }
-        sliders[currentElement-1].classList.toggle('.slider__body--show')
-        sliders[value-1].classList.toggle('.slider__body--show')
+        sliders[currentElement-1].classList.toggle('slider__body--show')
+        sliders[value-1].classList.toggle('slider__body--show')
     }
     
 })()
